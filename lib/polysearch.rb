7c5df7b69c57a46a47f51bf343327318bb575ab7
Hopsoft <natehop@gmail.com>
2021-05-24 12:55:16 -0600

    Reorganize to make zeitwork happy

--- lib/polysearch.rb
+++ lib/polysearch.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "polysearch/version"
-require_relative "../app/models/record"
-require_relative "../app/models/concerns/searchable"
+require_relative "../app/models/polysearch/record"
+require_relative "../app/models/polysearch/searchable"
 
 module Polysearch
   class Engine < Rails::Engine
